db221716841031d435aff131733229f1c0143215
James Cox <james@imaj.es>
2008-08-06 19:03:10 +0100

    fixing nil bug in stem and adding karma leaf stub

---
+++ leaves/karma/controller.rb
@@ -0,0 +1,10 @@
+# Controller for the Karma leaf.
+
+class Controller < Autumn::Leaf
+  
+  # Typing "!about" displays some basic information about this leaf.
+  
+  def about_command(stem, sender, reply_to, msg)
+    # This method renders the file "about.txt.erb"
+  end
+end
