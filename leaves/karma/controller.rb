f24b31861a2463016559c3e4f125c51d270e1f3b
James Cox <james@imaj.es>
2009-05-10 16:08:13 -0700

    deal with people who don't yet exist

--- leaves/karma/controller.rb
+++ leaves/karma/controller.rb
@@ -10,13 +10,13 @@ end
 
 class Controller < Autumn::Leaf
 
-   # def irc_privmsg_event(stem, sender, arguments)
-   #   #puts "the event is #{arguments[:channel]} <#{sender[:nick]}> #{arguments[:message]}"
-   # 
-   #   message = arguments[:message]
-   #   nick = sender[:nick]
-   #   
-   # end
+   def irc_privmsg_event(stem, sender, arguments)
+     #puts "the event is #{arguments[:channel]} <#{sender[:nick]}> #{arguments[:message]}"
+   
+     message = arguments[:message]
+     nick = sender[:nick]
+     
+   end
 
   
   # Typing "!about" displays some basic information about this leaf.
@@ -27,6 +27,13 @@ class Controller < Autumn::Leaf
   
   def karma_command(stem, sender, reply_to, msg)
     var :item => msg
-    var :score => Karma.first(:key => msg).value
+    
+    rv = Karma.first(:key => msg)
+    
+    unless rv.nil?
+      var :score => rv.value
+    else
+      var :score => nil
+    end
   end
 end
